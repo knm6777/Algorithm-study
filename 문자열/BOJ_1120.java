07f3b0249df1bc9b8554d016284305fa6bb93e0a
김민경 <mkkim@gimmingyeong-ui-iMac.local>
2022-05-28 01:56:26 +0900

    add [문자열]BOJ-1120

--- 문자열/BOJ_1120.java
+++ 문자열/BOJ_1120.java
@@ -1,2 +1,29 @@
-package 문자열;public class BOJ_1120 {
+package 문자열;
+
+import java.util.*;
+
+public class BOJ_1120 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        String A = in.next();
+        String B = in.next();
+
+        int ans = A.length();
+
+        for(int i=0; i<=B.length()-A.length(); i++) {
+            int tmp = 0;
+            for(int j=0; j<A.length(); j++) {
+                if (A.charAt(j) != B.charAt(i+j)){
+                    tmp++;
+                }
+            }
+            if(ans > tmp){
+                ans = tmp;
+            }
+        }
+        System.out.println(ans);
+        in.close();
+    }
 }
